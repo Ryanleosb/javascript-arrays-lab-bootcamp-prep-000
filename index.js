e4f8e169328d202e4c1b67ea5341bbf3ba069331
leo bang <ryanleosb@gmail.com>
2018-03-23 04:32:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,3 +10,9 @@ function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
 };
+
+function appendKitten(name){
+  var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
+};
