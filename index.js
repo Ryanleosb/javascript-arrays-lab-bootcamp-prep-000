b5cec5915cb6db23905bc8456067f5f1ce6df049
leo bang <ryanleosb@gmail.com>
2018-03-23 04:21:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
 const app = "I don't do much."
 var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten (array, newElement) {
+  var newArray = [newElement, ... array]
+  return newArray
+};
