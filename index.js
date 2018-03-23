399605ae40207c7e71a9c44e3bf861cc8ce6b7b3
leo bang <ryanleosb@gmail.com>
2018-03-23 04:43:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const app = "I don't do much."
-var kittens = ["Milo", "Otis", "Garfield",]
 
-function destructivelyAppendKitten (name){
-  kittens.push (name)
+var kittens = ["Milo", "Otis", "Garfield"]
+
+function destructivelyAppendKitten(name){
+  kittens.push(name)
   return kittens
-};
+}
 
 function destructivelyPrependKitten(name){
   kittens.unshift(name)
   return kittens
-};
+}
 
-function destructivelyRemoveLastKitten(name){
-  kittens.pop(name)
-  return kittens
-};
+function appendKitten(name){
+  var newKittens = [...kittens]
+  newKittens.push(name)
+  return newKittens
+}
 
-function destructivelyRemoveFirstKitten(name){
-  kittens.slice(name)
+function prependKitten(name){
+  var newKittens = [...kittens]
+  newKittens.unshift(name)
+  return newKittens
+}
+
+function destructivelyRemoveLastKitten(){
+  kittens.pop()
   return kittens
-};
+}
+
+function destructivelyRemoveFirstKitten(){
+  kittens.shift()
+  return kittens
+}
+
+function removeLastKitten(){
+  var newKittens = [...kittens]
+  newKittens.pop()
+  return newKittens
+}
+
+function removeFirstKitten(){
+  var newKittens = [...kittens]
+  newKittens.shift()
+  return newKittens
+}
